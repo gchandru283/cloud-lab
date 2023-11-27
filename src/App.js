3519d5ada61f535b9bd56684f496f3826c52c2e8
“gchandru283” <gchandru283@gmail.com>
2023-11-27 17:22:36 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  const resumeUrl = process.env.PUBLIC_URL + '/files/cloud-lab.pdf';
+  return (
+    <div className="h-screen">
+      <div className="p-5 font-mono fixed w-full">
+     <a href={resumeUrl} target="_blank" rel="noreferrer noopener" download="Cloud lab.pdf">Hello World!</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
